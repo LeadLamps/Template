e43bd5f0d5f963785d26a16069d335e5d5892d15
Anderson Martins dos Santos <foxvc22@gmail.com>
2018-11-06 17:57:34 -0200

    Add files via upload

---
+++ Template/app/controllers/controller.js
@@ -0,0 +1,12 @@
+app.controller('HomeCtrl', function($scope){
+	
+	$scope.title = 'Bem vindo, esta é a página principal';
+		
+});
+app.controller('ContactCtrl', function($scope){
+    $scope.title = 'Entre em contato conosco!';
+});
+
+app.controller('AboutCtrl', function($scope){
+    $scope.title = 'Olá, esta é a página sobre da empresa!';
+});
---
+++ Template/app/routes/route.js
@@ -0,0 +1,20 @@
+app.config(function($routeProvider){
+
+    $routeProvider
+    .when('/', {
+        templateUrl : '../app/views/home.html',
+        controller  : 'HomeCtrl',
+    })
+
+    .when('/about', {
+        templateUrl : '../app/views/about.html',
+        controller  : 'AboutCtrl',
+    })
+
+    .when('/contact', {
+        templateUrl : '../app/views/contact.html',
+        controller  : 'ContactCtrl',
+    })
+
+    .otherwise ({ redirectTo: '/' });
+});
